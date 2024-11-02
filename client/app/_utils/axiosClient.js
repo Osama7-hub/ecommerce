98094880e1ba91f651e767a3ddd83e7b6219585b
Osama Ahmed <osama.des795@gmail.com>
2024-11-02 20:16:49 +0300

    some edit

--- client/app/_utils/axiosClient.js
+++ client/app/_utils/axiosClient.js
@@ -1,7 +1,8 @@
 const {default: axios} = require('axios')
 
 const apiKey=process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl='http://localhost:1337/api'
+// const apiUrl='http://localhost:1337/api'
+const apiUrl='https://ecommerce-qcj6.onrender.com/api'
 
 const axiosClient = axios.create({
 	baseURL: apiUrl,
